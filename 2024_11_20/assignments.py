eb2d37a7cd23cb9e21fbf50bf94d54ce20fd336e
yumemonzo <yumemonzo@gmail.com>
2024-11-15 09:53:23 +0000

    Add: Initial commit

--- 2024_11_20/assignments.py
+++ 2024_11_20/assignments.py
@@ -13,9 +13,9 @@ def main():
     train_dataset, test_dataset = get_datasets(transform=transform)
     train_dataset, valid_dataset = split_dataset(dataset=train_dataset, split_size=0.8)
 
-    train_loader = DataLoader(dataset=train_dataset, batch_size=256, shuffle=True)
-    valid_loader = DataLoader(dataset=valid_dataset, batch_size=256, shuffle=False)
-    test_loader = DataLoader(dataset=test_dataset, batch_size=256, shuffle=False)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+    valid_loader = DataLoader(dataset=valid_dataset, batch_size=64, shuffle=False)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
 
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -25,7 +25,7 @@ def main():
     criterion = nn.CrossEntropyLoss()
 
     trainer = Trainer(model, train_loader, valid_loader, criterion, optimizer, device, save_path="best_model.pth")
-    trainer.training(num_epochs=5)
+    trainer.training(num_epochs=50)
 
     test_loss, test_acc = trainer.test(test_loader)
     print(f"Test Loss: {test_loss:.4f}, Test Accuracy: {test_acc:.4f}")
