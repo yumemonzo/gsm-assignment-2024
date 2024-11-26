28e26c1a574ce50c9fa199d70bf4e771b3810f19
yumemonzo <yumemonzo@gmail.com>
2024-11-26 05:07:54 +0000

    Add: Initial commit

---
+++ grade_2/KimDongYeon/2024_11_26/assignment/model.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 8 * 8)
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        output = self.fc2(x)
+
+        return output
+    
